efc0b277871639d5190d01e0ac04a77cf3d4d4e6
Warrick Moran <warrick.moran@localhost.localdomain>
2019-03-21 15:06:38 -0400

    general cleanup

--- src/main/java/gov/noaa/idss/cap/ApplicationConfiguration.java
+++ src/main/java/gov/noaa/idss/cap/ApplicationConfiguration.java
@@ -1,14 +1,8 @@
 package gov.noaa.idss.cap;
 
-import org.apache.camel.converter.jaxb.JaxbDataFormat;
-import org.apache.camel.spi.DataFormat;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfiguration {
-	@Bean
-	public DataFormat jaxb() {
-		return new JaxbDataFormat("oasis.names.tc.emergency.cap._1");
-	}
+	
 }
